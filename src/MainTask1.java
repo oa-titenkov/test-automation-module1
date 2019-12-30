adf0b69396bc4bed9433b2530443bec253f0d926
sohumble <lightning07@yandex.ru>
2019-12-30 15:34:37 +0300

    mainTask1

---
+++ src/MainTask1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MainTask1 {
+
+  public static void main(String args[]) {
+
+    Scanner input = new Scanner(System.in);
+    String name = input.nextLine();
+    System.out.println("Hello " + name);
+
+  }
+
+}
