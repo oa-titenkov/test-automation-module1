7cc6f7469f0e3e5dde4ebc981015cfb68630939d
sohumble <lightning07@yandex.ru>
2019-12-30 16:59:16 +0300

    opt2

--- src/OptionalTask2.java
+++ src/OptionalTask2.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -10,9 +12,32 @@ public class OptionalTask2 {
     int[][] matrix = new int[n][n];
     for(int i = 0; i < n; i++){
       for(int j = 0; j < n; j++){
-        matrix[i][j] = new Random().nextInt();
-        System.out.println(matrix[i][j]);
+        matrix[i][j] = new Random().nextInt(100);
+        System.out.print(matrix[i][j]+" ");
       }
+      System.out.print("\n");
+    }
+
+    System.out.println("Введите номер столбца:");
+    int column = input.nextInt();
+    Arrays.sort(matrix, Comparator.comparingInt(a -> a[column-1])); //по k-столбцу
+    System.out.println("Sorted by column:");
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < n; j++){
+        System.out.print(matrix[i][j]+" ");
+      }
+      System.out.print("\n");
+    }
+
+    System.out.println("Введите номер строки:");
+    int row = input.nextInt();
+    Arrays.sort(matrix, (a, b) -> Double.compare(a[0], b[0])); //по k-столбцу
+    System.out.println("Sorted by row:");
+    for(int i = 0; i < n; i++){
+      for(int j = 0; j < n; j++){
+        System.out.print(matrix[i][j]+" ");
+      }
+      System.out.print("\n");
     }
 
   }
