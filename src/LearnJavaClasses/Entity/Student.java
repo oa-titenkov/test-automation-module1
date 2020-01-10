c141cdb8c5b4e163883428e00376ebc2726d18de
sohumble <lightning07@yandex.ru>
2020-01-10 08:50:34 +0300

    java classes student

---
+++ src/LearnJavaClasses/Entity/Student.java
@@ -0,0 +1,145 @@
+package LearnJavaClasses.Entity;
+
+import java.time.LocalDate;
+
+public class Student {
+
+  private int id;
+  private String lastName;
+  private String firstName;
+  private String middleName;
+  private LocalDate birthday;
+  private String address;
+  private String phoneNumber;
+  private String faculty;
+  private int course;
+  private String group;
+
+  public Student(int id, String firstName, String lastName, String faculty, String birthday, int course, String group) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.faculty = faculty;
+    String[] birthdayArray  = birthday.split("\\.");
+    this.birthday = LocalDate.of(Integer.parseInt(birthdayArray[2]), Integer.parseInt(birthdayArray[1]), Integer.parseInt(birthdayArray[0]));
+    this.course = course;
+    this.group = group;
+  }
+
+  public Student(int id, String lastName, String firstName, String middleName) {
+    this.id = id;
+    this.lastName = lastName;
+    this.firstName = firstName;
+    this.middleName = middleName;
+  }
+
+  public Student(int id, String lastName, String firstName) {
+    this.id = id;
+    this.lastName = lastName;
+    this.firstName = firstName;
+  }
+
+  public Student(int id, String lastName, String firstName, String middleName, String faculty) {
+    this.id = id;
+    this.lastName = lastName;
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.faculty = faculty;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public void setMiddleName(String middleName) {
+    this.middleName = middleName;
+  }
+
+  public LocalDate getBirthday() {
+    return birthday;
+  }
+
+  public void setBirthday(LocalDate birthday) {
+    this.birthday = birthday;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getFaculty() {
+    return faculty;
+  }
+
+  public void setFaculty(String faculty) {
+    this.faculty = faculty;
+  }
+
+  public int getCourse() {
+    return course;
+  }
+
+  public void setCourse(int course) {
+    this.course = course;
+  }
+
+  public String getGroup() {
+    return group;
+  }
+
+  public void setGroup(String group) {
+    this.group = group;
+  }
+
+  @Override
+  public String toString() {
+    return "Student{" +
+            "id=" + id +
+            ", lastName='" + lastName + '\'' +
+            ", firstName='" + firstName + '\'' +
+            ", middleName='" + middleName + '\'' +
+            ", birthday=" + birthday +
+            ", address='" + address + '\'' +
+            ", phoneNumber='" + phoneNumber + '\'' +
+            ", faculty='" + faculty + '\'' +
+            ", course=" + course +
+            ", group='" + group + '\'' +
+            '}';
+  }
+}
--- src/OptionalTask2.java
+++ src/OptionalTask2.java
@@ -6,40 +6,77 @@ import java.util.Scanner;
 public class OptionalTask2 {
 
   public static void main(String[] args) {
-    Scanner input  = new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
     System.out.print("Введите размерность матрицы: ");
     int n = input.nextInt();
     int[][] matrix = new int[n][n];
-    for(int i = 0; i < n; i++){
-      for(int j = 0; j < n; j++){
-        matrix[i][j] = new Random().nextInt(100);
-        System.out.print(matrix[i][j]+" ");
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        matrix[i][j] = new Random().nextInt(200) - 100;
+        System.out.print(matrix[i][j] + " ");
       }
       System.out.print("\n");
     }
+
+    //Task1
 
     System.out.println("Введите номер столбца:");
     int column = input.nextInt();
-    Arrays.sort(matrix, Comparator.comparingInt(a -> a[column-1])); //по k-столбцу
+    Arrays.sort(matrix, Comparator.comparingInt(a -> a[column - 1])); //по k-столбцу
     System.out.println("Sorted by column:");
-    for(int i = 0; i < n; i++){
-      for(int j = 0; j < n; j++){
-        System.out.print(matrix[i][j]+" ");
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        System.out.print(matrix[i][j] + " ");
       }
       System.out.print("\n");
     }
 
-    System.out.println("Введите номер строки:");
-    int row = input.nextInt();
-    Arrays.sort(matrix, (a, b) -> Double.compare(a[0], b[0])); //по k-столбцу
-    System.out.println("Sorted by row:");
-    for(int i = 0; i < n; i++){
-      for(int j = 0; j < n; j++){
-        System.out.print(matrix[i][j]+" ");
-      }
-      System.out.print("\n");
-    }
+//    System.out.println("Введите номер строки:");
+//    int row = input.nextInt();
+//    Arrays.sort(matrix, (a, b) -> Double.compare(a[0], b[0])); //по k-столбцу
+//    System.out.println("Sorted by row:");
+//    for (int i = 0; i < n; i++) {
+//      for (int j = 0; j < n; j++) {
+//        System.out.print(matrix[i][j] + " ");
+//      }
+//      System.out.print("\n");
+//    }
 
+
+    //Task2
+    int ascNumber = 0;
+    int ascMax = 0;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n - 1; j++) {
+        if (matrix[i][j] < matrix[i][j + 1]) {
+          ascNumber++;
+        } else if (matrix[i][j] >= matrix[i][j + 1]) {
+          if (ascNumber > ascMax) {
+            if (ascNumber > ascMax) {
+              ascMax = ascNumber;
+              ascNumber = 0;
+            } else ascNumber = 0;
+          }
+        }
+      }
+      System.out.println("Max ascending numbers count: " + ascMax);
+
+      //Task3
+
+      for (int i = 0; i < n; i++) {
+        int rowSum = 0;
+        int positiveCounter = 0;
+        for (int j = 0; j < n; j++) {
+          if (matrix[i][j] > 0 && positiveCounter < 2) {
+            positiveCounter++;
+          } else if (matrix[i][j] < 0 && positiveCounter < 2 && positiveCounter != 0) {
+            rowSum += matrix[i][j];
+          }
+        }
+        System.out.println("Sum on row " + (i + 1) + " = " + rowSum);
+      }
+
+    }
   }
 
 }
