1aa3d5c4b283e0f1ba4f8f3da009580ba921d852
sohumble <lightning07@yandex.ru>
2020-01-10 10:37:45 +0300

    java classes patient

---
+++ src/LearnJavaClasses/Action/PatientAction.java
@@ -0,0 +1,39 @@
+package LearnJavaClasses.Action;
+
+import LearnJavaClasses.Entity.Patient;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class PatientAction {
+
+  public static void main(String[] args) {
+    List<Patient> patientList = new ArrayList<>();
+    long medicalCardNumberRange = 10000000000000L;
+    patientList.add(new Patient(0, "Oleg", "Titenkov", ThreadLocalRandom.current().nextLong(medicalCardNumberRange), "flu"));
+    patientList.add(new Patient(1, "Andrei", "Popel", ThreadLocalRandom.current().nextLong(medicalCardNumberRange), "healthy"));
+    patientList.add(new Patient(2, "Olga", "Makarenko", ThreadLocalRandom.current().nextLong(medicalCardNumberRange), "healthy"));
+    patientList.add(new Patient(3, "Andrei", "Loboiko", ThreadLocalRandom.current().nextLong(medicalCardNumberRange), "healthy"));
+    patientList.add(new Patient(4, "Anton", "Savchkov", ThreadLocalRandom.current().nextLong(medicalCardNumberRange), "healthy"));
+//    getPatientsByDiagnosis(patientList, "healthy");
+//    getCustomerListByCreditCard(patientList, 5000000000000L, 9000000000000L);
+  }
+
+  private static void getPatientsByDiagnosis(List<Patient> patientList, String diagnosis) {
+    for(Patient patient : patientList){
+      if(patient.getDiagnosis().equals(diagnosis)){
+        System.out.println(patient.toString());
+      }
+    }
+  }
+
+  private static void getCustomerListByCreditCard(List<Patient> patientList, long min, long max) {
+    for(Patient patient : patientList) {
+      if(patient.getMedicalCardNumber() >= min && patient.getMedicalCardNumber() <= max) {
+        System.out.println(patient.toString());
+      }
+    }
+  }
+
+}
