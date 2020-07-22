d4989fe6ced0fe471cd24683b733724242329146
oa-titenkov <60258143+oa-titenkov@users.noreply.github.com>
2020-07-22 09:15:22 +0300

    fixed style

--- src/main/java/module1/fundamentals/MainTask4.java
+++ src/main/java/module1/fundamentals/MainTask4.java
@@ -5,10 +5,13 @@ public class MainTask4 {
   public static void main(String args[]) {
     int sum = 0;
     int multiply = 1;
+    
     for(String item : args){
       sum += Integer.parseInt(item);
       multiply *= Integer.parseInt(item);
     }
+    
     System.out.println("sum = " + sum + ", multiply = " + multiply);
   }
+  
 }
